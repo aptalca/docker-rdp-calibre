12c5b14943ae36f1cc241ff66c6b22f90b44b847
aptalca <thedjinn@yahoo.com>
2015-04-27 22:36:50 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo -e "calibre\ncalibre" | passwd root
+mkdir -p /config/config
+ln -s /config/config /root/.config/calibre
 service xrdp restart
 calibre-server --with-library=/config &
