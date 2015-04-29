35b64406b462429d6de4da90009bb3253ca438a7
aptalca <thedjinn@yahoo.com>
2015-04-29 19:33:16 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set the correct local time
+echo $TZ > /etc/timezone
+export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
+dpkg-reconfigure tzdata
+
+# Generate root password for RDP
 echo -e "calibre\ncalibre" | passwd root
 mkdir -p /config/config
 ln -s /config/config /root/.config/calibre
