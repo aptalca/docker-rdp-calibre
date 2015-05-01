38886103ebefe7bf650cd0aba268fb977ec81b00
aptalca <thedjinn@yahoo.com>
2015-05-01 16:56:12 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -3,4 +3,4 @@
 mkdir -p /config/config
 ln -s /config/config /nobody/.config/calibre
 chown -R nobody:users /config
-su -c "calibre-server --with-library=/config &" -s /bin/sh nobody
+su -c "calibre-server --with-library=/config --port 8081 &" -s /bin/sh nobody
