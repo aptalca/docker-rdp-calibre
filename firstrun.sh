537cb88e1cc0c483c9379cbb1dd36b73abcb69d8
aptalca <thedjinn@yahoo.com>
2015-05-10 15:19:41 -0400

    EDGE setting added

--- firstrun.sh
+++ firstrun.sh
@@ -3,4 +3,12 @@
 mkdir -p /config/config
 ln -s /config/config /nobody/.config/calibre
 chown -R nobody:users /config
+
+if [ ! "$EDGE" = "1" ]; then
+  echo "EDGE not requested, keeping stable version"
+else
+  echo "EDGE requested, updating to latest version"
+  wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+fi
+
 su -c "calibre-server --with-library=/config --port 8081 &" -s /bin/sh nobody
