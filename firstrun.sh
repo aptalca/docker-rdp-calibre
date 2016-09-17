273a70ad5c46c2e375b1dd14b5e0ec03a4f64216
aptalca <aptalca@users.noreply.github.com>
2016-09-16 23:58:11 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -4,7 +4,7 @@ mkdir -p /config/config
 ln -s /config/config /nobody/.config/calibre
 chown -R nobody:users /config
 chmod -R g+rw /config
-[[ -f /tmp/.X1-lock ]] && rm /tmp/.X1-lock
+[[ -f /tmp/.X1-lock ]] && rm /tmp/.X1-lock && echo "X1-lock found, deleting"
 
 if [ ! "$EDGE" = "1" ]; then
   echo "EDGE not requested, keeping stable version"
