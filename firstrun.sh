49310a1df96caa3f7502a11696347273f91a3470
aptalca <aptalca@users.noreply.github.com>
2017-06-24 00:48:37 -0400

    don't start the webserver

--- firstrun.sh
+++ firstrun.sh
@@ -12,9 +12,3 @@ else
   echo "EDGE requested, updating to latest version"
   wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 fi
-
-if [ -z "$LIBRARYINTERNALPATH" ]; then
-  LIBRARYINTERNALPATH=/config
-fi
-
-/sbin/setuser nobody calibre-server --with-library=$LIBRARYINTERNALPATH --port 8081 --url-prefix=$URLPREFIX &
